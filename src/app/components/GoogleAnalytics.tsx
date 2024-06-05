e6da5c259c22d86cb20e45f17b17e1ca8bd6e0b6
shrijalkaphle <kaphle.shrijal9@gmail.com>
2024-06-05 09:23:28 +0545

    ``feat:`` :sparkles: added google analytics

--- src/app/components/GoogleAnalytics.tsx
+++ src/app/components/GoogleAnalytics.tsx
@@ -6,12 +6,10 @@ import Script from 'next/script';
 const GoogleAnalytics = () => {
   return (
     <>
-      <Script
-        strategy='lazyOnload'
-        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_MEASUREMENT_ID}`}
+      <Script src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_MEASUREMENT_ID}`}
       />
 
-      <Script id='' strategy='lazyOnload'>
+      <Script id=''>
         {`
               window.dataLayer = window.dataLayer || [];
               function gtag(){dataLayer.push(arguments);}

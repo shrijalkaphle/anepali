bffd8cc47eeec1d27bc627b45b41e7e44f64b370
shrijalkaphle <kaphle.shrijal9@gmail.com>
2024-06-05 09:22:42 +0545

    ``feat:`` :sparkles: added google analytics

--- src/app/components/GoogleAnalytics.tsx
+++ src/app/components/GoogleAnalytics.tsx
@@ -16,9 +16,7 @@ const GoogleAnalytics = () => {
               window.dataLayer = window.dataLayer || [];
               function gtag(){dataLayer.push(arguments);}
               gtag('js', new Date());
-              gtag('config', '${process.env.NEXT_PUBLIC_MEASUREMENT_ID}', {
-              page_path: window.location.pathname,
-              });
+              gtag('config', '${process.env.NEXT_PUBLIC_MEASUREMENT_ID}');
           `}
       </Script>
     </>

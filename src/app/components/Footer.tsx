199ebff988f9f44d1af1c674d34539c6e14e21dc
Shrijal Kaphle <kaphle.shrijal9@gmail.com>
2024-06-29 21:10:18 +0545

    ``bug:`` :bug: fix font-szie error

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -3,15 +3,6 @@ import Link from "next/link"
 export const Footer = () => {
     return (
         <footer>
-            <div className="container mt-20 my-10">
-                {/* <div className="rounded-lg bg-neutral-50 h-[136px] flex items-center justify-center">
-                    Ad Space
-                </div> */}
-                <div className="mt-10 rounded-lg bg-neutral-50 p-4">
-                    The fonts presented on this website are their authors&#39; property, and are either freeware, shareware, demo versions or public domain. Please look at the readme-files in the archives or check the indicated author&#39;s website for details, and contact him/her if in doubt. If no author/license is indicated that&#39;s because we don&#39;t have information, that doesn&#39;t mean it&#39;s free.
-                </div>
-            </div>
-            <hr />
             <div className="container my-10 lg:flex justify-between">
                 <div className="lg:w-1/3 w-full">
                     <img src="/assets/logo.svg" alt="aNepali" className="h-8" />

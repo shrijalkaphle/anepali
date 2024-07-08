b184b1b3eb7663a5437e45d913706d6666a0b8e3
Shrijal Kaphle <kaphle.shrijal9@gmail.com>
2024-07-08 21:13:16 +0545

    ``bug:`` :bug: updated img to next/Image

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image"
 import Link from "next/link"
 
 export const Footer = () => {
@@ -5,7 +6,7 @@ export const Footer = () => {
         <footer>
             <div className="container my-10 lg:flex justify-between">
                 <div className="lg:w-1/3 w-full">
-                    <img src="/assets/logo.svg" alt="aNepali" className="h-8" />
+                    <Image src="/assets/logo.svg" alt="aNepali" height={32} width={149} />
                     <span className="mt-5 block">aNepali is a platform dedicated to improving experience of Nepalese community in the Internet. </span>
                 </div>
                 <div className="lg:flex justify-between lg:w-1/2 w-full mt-10 lg:mt-0">
